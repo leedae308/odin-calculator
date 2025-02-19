2bb13778abc289e23b3455415bc7bf42e8ca110e
Daehee Lee <daehee.lee308@gmail.com>
2025-02-19 16:22:21 +0900

    add small display function

--- functions.js
+++ functions.js
@@ -1,14 +1,16 @@
 const display = document.querySelector(".screen");
 const numbers = document.querySelectorAll(".cyan");
+const smallDisplay = document.querySelector(".smallDisplay");
+
 // const calculator = document.querySelector(".calculator");
-let cleared=true;
+let cleared = true;
 
 numbers.forEach((num) => {
     num.addEventListener("click", function () {
-        if(!cleared)
-        {
-            display.textContent="";
-            cleared=true;
+        if (!cleared) {
+            display.textContent = "";
+            cleared = true;
+            // smallDisplay.textContent = "";
         }
         display.textContent += num.textContent;
         // calculator.appendChild(display);
@@ -22,8 +24,8 @@ operators.forEach((oper) => {
     oper.addEventListener("click", function () {
         variable1 = display.textContent;
         operator = oper.textContent;
-        cleared=false;
-
+        cleared = false;
+        smallDisplay.textContent += variable1 + " " + operator;
         // console.log(variable1);
         // console.log(operator);
     })
@@ -43,8 +45,10 @@ equal.addEventListener("click", function () {
     variable2 = display.textContent;
     // console.log(typeof (variable1), variable2, typeof (operator));
     display.textContent = operate(variable1, variable2, operator);
+    smallDisplay.textContent+= " " + variable2;
     variable1 = null;
     variable2 = null;
     operator = null;
-    cleared=false;
-})
+    cleared = false;
+});
+
